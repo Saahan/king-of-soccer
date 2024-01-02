757fc7872de9167fc17a2da5e44e413fff4f980a
Saahan Mombasawala <smombasa1989@gmail.com>
2024-01-02 10:38:18 +0530

    change image load for start.js

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,32 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./start.css";
 
 export default function Start(props) {
+  const [imgLoaded, setImgLoaded] = useState(false);
+
   function nextLevel(e) {
     props.nextLevel(e);
   }
 
+  const img = new Image();
+  img.onload = () => {
+    setImgLoaded(true);
+  };
+  img.src = "/img/commentrator.png";
+
   return (
-    <div className="center-div">
-      <div>
-        <img src="/img/commentrator.png" alt="commentrator"></img>
+    imgLoaded === true && (
+      <div className="center-div">
+        <div>
+          <img src="/img/commentrator.png" alt="commentrator"></img>
+        </div>
+
+        <div>
+          <p>
+            "Welcome to the CHAMPIONS CUP Finals! Can the underdogs RAZOR SHARP
+            UNITED beat tested champions TECMO BOYS? HAMSUN of the RAZOR's is in
+            great form, primed to create history at this coveted competition!"
+          </p>
+        </div>
+        <div
+          className="button"
+          onClick={() => {
+            nextLevel("Prelude");
+          }}
+        >
+          Continue
+        </div>
       </div>
-      <div>
-        <p>
-          "Welcome to the CHAMPIONS CUP Finals! Can the underdogs RAZOR
-          SHARP UNITED beat tested champions TECMO BOYS? HAMSUN of the RAZOR's
-          is in great form, primed to create history at this coveted
-          competition!"
-        </p>
-      </div>
-      <div
-        className="button"
-        onClick={() => {
-          nextLevel("Prelude");
-        }}
-      >
-        Continue
-      </div>
-    </div>
+    )
   );
 }
